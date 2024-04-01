de5f4bd8102ae69fc003a0976afb153bf249a9a5
Muhammad faheem <160300731+Faheem0988@users.noreply.github.com>
2024-04-01 12:53:45 -0700

    Add files via upload

---
+++ Your_Own_Array.ts
@@ -0,0 +1,9 @@
+
+
+let transportation :string[] = ['cycle','motorcycle','car','train'] ;
+
+for(let i=0; i<transportation.length; i++){
+
+    console.log('I would like to won a ' + transportation [i])
+
+}
