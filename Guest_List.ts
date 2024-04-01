850e75be2dd709ee463a8711f0cba3ef13d4b490
Muhammad faheem <160300731+Faheem0988@users.noreply.github.com>
2024-04-01 12:54:16 -0700

    Add files via upload

---
+++ Guest_List.ts
@@ -0,0 +1,9 @@
+
+
+let Guest_List :string[] = ['fayyaz','ayyaz','farhan'];
+
+for(let i=0; i<Guest_List.length; i++){
+
+    console.log('Dear Mr. ' +Guest_List[i] +',\n\mIt is our pleasure to invite you in our party.\n\nThank You!')
+
+}
