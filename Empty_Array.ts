43cb6c62b3f8d4b69329bc2e3bfcc15d811eb61c
Muhammad faheem <160300731+Faheem0988@users.noreply.github.com>
2024-04-01 13:06:39 -0700

    Add files via upload

---
+++ Empty_Array.ts
@@ -0,0 +1,16 @@
+
+
+// Think of something you could store in a array.
+let items :string[] = ['Pakistan','Chaina','Iran'];
+
+console.log(items);
+
+//  Write a program that creates a list containing these items.
+
+let item2 :string[] = [];
+
+item2.push('Germany');
+item2.push('England');
+item2.push('japan');
+
+console.log(item2);
