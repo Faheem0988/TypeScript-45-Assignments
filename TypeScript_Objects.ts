2fe90bf626f8eed5e101d568854f993b0afb7962
Muhammad faheem <160300731+Faheem0988@users.noreply.github.com>
2024-04-01 13:07:12 -0700

    Add files via upload

---
+++ TypeScript_Objects.ts
@@ -0,0 +1,35 @@
+
+
+// Write a program that creates Objects containing these items
+
+// dara type of person object
+interface person {
+    age : number,
+    name : string,
+    nationality : string,
+    student : Boolean
+}
+// person object
+let person :person = {
+    age : 35 ,
+    name : 'Faheem' ,
+    nationality : 'Pakistan' ,
+    student : true
+}
+// print person
+console.log (person);
+
+// datatype car object
+interface car {
+    maker : string,
+    color : string,
+    automatic : boolean 
+}
+// car boject
+let car = {
+    maker : 'Toyota' ,
+    color : 'Black' ,
+    automatic : true 
+}
+// print car
+console.log (car);
