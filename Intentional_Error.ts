6d1375262fa37d49a97a68a5efd37ff69ba26f59
Muhammad faheem <160300731+Faheem0988@users.noreply.github.com>
2024-04-01 13:07:39 -0700

    Add files via upload

---
+++ Intentional_Error.ts
@@ -0,0 +1,11 @@
+// If you haven’t received an array index error in one of your programs yet, try to make one happen.
+
+// creating array
+let newArray :string[] = ['Muhammad Faheem','Muhammad Fayaz','Muhammad Ayaz'];
+
+// accessing array by index
+console.log(newArray[5]);
+
+// Make sure you correct the error before closing the program.
+// Correct the array
+console.log(newArray);
