fe8a275ba27db45d4de80fb6dc931f87e579fb49
Muhammad faheem <160300731+Faheem0988@users.noreply.github.com>
2024-04-01 12:53:15 -0700

    Add files via upload

--- Names.ts
+++ Names.ts
@@ -2,6 +2,9 @@
 
 let Names :string[] = ['faheem','fayyaz','ayaz','farhan'] ;
 
+const message :string = 'Welcome to IT world:'
+
 for(let i=0; i<Names.length; i++){
-    console.log(Names[i]) ;
+
+    console.log(message + Names[i]) ;
 }
