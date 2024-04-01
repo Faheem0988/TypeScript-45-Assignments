14cbe7eeecbe5d7eac9536867429ac5f89c304a2
Muhammad faheem <160300731+Faheem0988@users.noreply.github.com>
2024-04-01 12:48:22 -0700

    Add files via upload

---
+++ Stripping_Names.ts
@@ -0,0 +1,8 @@
+
+const personName :string = '   \n\tMuhammad Faheem\t\n   ';
+
+console.log(personName);
+
+const without_whitespace :string = personName.trim();
+
+console.log(without_whitespace);
